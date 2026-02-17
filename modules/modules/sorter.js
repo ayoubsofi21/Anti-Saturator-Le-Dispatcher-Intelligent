3f09e2b016dc26949b53dad257deed22fc15d487
ayoubsofi <ayoubsofi03@email.com>
2026-02-17 12:52:36 +0000

    Préparer la structure des tâches

---
+++ modules/modules/sorter.js
@@ -0,0 +1,5 @@
+const tasks = [
+  { id: 1, title: "Tâche A", urgency: 5, importance: 5, effort: 2 },
+  { id: 2, title: "Tâche B", urgency: 3, importance: 4, effort: 1 },
+  { id: 3, title: "Tâche C", urgency: 4, importance: 4, effort: 2 },
+];
