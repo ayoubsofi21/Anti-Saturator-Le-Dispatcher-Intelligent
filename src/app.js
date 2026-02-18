d526e7f2a84f4b3ba24ddf4178b8eb5bfa66e2e7
ayoubsofi <ayoubsofi03@email.com>
2026-02-18 15:22:08 +0000

    build function of adapt Score to Energy

--- src/app.js
+++ src/app.js
@@ -1,10 +1,22 @@
-import { calculateScore } from "./modules/sorter.js";
+import { calculateScore, adaptScoreToEnergy } from "./modules/sorter.js";
 
-const tasks = [
-  { id: 1, title: "Tâche A", urgency: 5, importance: 5, effort: 2 },
-  { id: 2, title: "Tâche B", urgency: 3, importance: 4, effort: 1 },
-  { id: 3, title: "Tâche C", urgency: 4, importance: 4, effort: 2 },
-];
-tasks.forEach((task) => {
-  console.log(task.title + "(Score): " + calculateScore(task));
-});
+// const tasks = [
+//   { id: 1, title: "Tâche A", urgency: 5, importance: 5, effort: 2 },
+//   { id: 2, title: "Tâche B", urgency: 3, importance: 4, effort: 1 },
+//   { id: 3, title: "Tâche C", urgency: 4, importance: 4, effort: 2 },
+// ];
+// tasks.forEach((task) => {
+//   console.log(task.title + "(Score): " + calculateScore(task));
+// });
+const hardTask = {
+  id: 2,
+  title: "Big Project",
+  urgency: 5,
+  importance: 5,
+  effort: 5,
+};
+// tasks.forEach((task) => {
+//   console.log(task.title + "(Score): " + calculateScore(task));
+// });
+console.log("(Score): " + calculateScore(hardTask));
+console.log("Low energy:", adaptScoreToEnergy(hardTask, 4));
