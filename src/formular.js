ba037df2db667f402a239ddb13a68e4a4d807d9e
Hamza <hamzazaidi253@gmail.com>
2026-02-18 16:07:33 +0000

    cree file formular.js pour partie dynamique de projet

---
+++ src/formular.js
@@ -0,0 +1,7 @@
+let title = document.getElementById("task-title");
+let urgence = document.getElementById ("task-urgence");
+let importance = document.getElementById("task-importance");
+let effort = document.getElementById("task-effort");
+let button = document.getElementById("add-btn");
+
+let tasks = [];
