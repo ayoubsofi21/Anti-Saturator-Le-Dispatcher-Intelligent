c8eb9e59998e211380965ad87fe13b60e42751b1
Wassim Mansoury <wassimelmansoury@gmail.com>
2026-02-19 15:06:42 +0000

    implémenter la priorité des tâches et corriger les erreurs de calcul

--- modules/modules/sorter.js
+++ modules/modules/sorter.js
@@ -1,3 +1,25 @@
-export function getTopTaskOptimized(tasks) {
-  if (tasks.length === 0) return null;
+// function calculateScore( task ){
+//     const imortanceWeight = task.importance * 10;
+//     const urgencyWeight = task.urgency * 5;
+//     return importanceWeight + urgencyWeight;
+// }
+
+export function getTopTaskOptimized(tasks){
+    if ( tasks.length === 0) return null;
+
+let bestTask = tasks[0];
+let bestScore = calculateScore(tasks[i]);
+
+for(let i = 1 ; i < tasks.length; i++){
+    const currenteScore = calculateScore(tasks[i]);
+
+    if (currenteScore > bestScore ||
+        (currenteScore === bestScore && 
+            tasks[i].importance > bestTask.importance)
+        ){
+            bestTask = tasks[i];
+            bestScore = currenteScore;
+    }
+}
+return bestTask;
 }
