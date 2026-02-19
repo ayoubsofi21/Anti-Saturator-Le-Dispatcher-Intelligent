c543f6fcde05b72effab34cefa81588b6ecfd313
Wassim Mansoury <wassimelmansoury@gmail.com>
2026-02-19 11:44:14 +0000

    exportation de la fonction principale getTopTask

---
+++ app.js
@@ -0,0 +1,16 @@
+let numbs = [1,2,3,4];
+function calSum(arr){
+    let sum=0;
+    for(i=0; i<arr.length; i++){i
+        sum+=arr[];
+    }
+    return sum;
+}
+console.log(calSum(numbs));
+ 
+numbs.sort((a,b)=> b-a);
+console.log(numbs);
+
+
+numbs.sort((a,b)=> (b-a));
+console.log(Number);
---
+++ modules/modules/sorter.js
@@ -0,0 +1,3 @@
+export function getTopTaskOptimized(tasks) {
+  if (tasks.length === 0) return null;
+}
